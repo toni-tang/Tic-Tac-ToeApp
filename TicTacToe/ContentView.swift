32a28f6f6fa8f9500c2ac716a22bcef3c7ff7cbe
Tony Tang <tonytang213@gmail.com>
2022-05-12 19:29:33 -0500

    Initial Commit

---
+++ TicTacToe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Tony Tang on 5/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Tony Tang on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
