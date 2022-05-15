f31c55c2dc634ab110dfd759b8589cb94dd6f2af
Tony Tang <tonytang213@gmail.com>
2022-05-15 13:43:08 -0500

    Initial UI

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+let columns: [GridItem]  = [GridItem(.flexible()),
+                            GridItem(.flexible()),
+                            GridItem(.flexible())]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+            BackgroundView()
+            LazyVGrid(columns: columns){
+                BoxView()
+                BoxView()
+                BoxView()
+                BoxView()
+                BoxView()
+                BoxView()
+                BoxView()
+                BoxView()
+                BoxView()
+            }
             .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BoxView: View {
+    var body: some View {
+        Button{
+            print(5)
+        } label: {
+            Text("")
+                .frame(width: 100, height: 100)
+                .background(.pink)
+                .foregroundColor(.white)
+                .cornerRadius(50)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [Color("salmon"), Color("lightPink")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .ignoresSafeArea(.all)
     }
 }
